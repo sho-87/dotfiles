d0a60a63f4380cc84869f47c41c8f95788ec75d2
Simon Ho <simonsays87@googlemail.com>
2017-04-09 02:40:29 -0700

    Old spacemacs config

---
+++ setup_scripts/spacemacs_setup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
+
+cd ~
+
+ln -s /Users/simon/Dropbox/dotfiles/.spacemacs .spacemacs
---
+++ setup_scripts/spacemacs_windows_setup.bat
@@ -0,0 +1,8 @@
+set dropbox=D:/Dropbox/dotfiles
+set homeDir=C:/Users/%USERNAME%
+
+setx HOME %homeDir%
+
+git clone https://github.com/syl20bnr/spacemacs %homeDir%/.emacs.d
+
+mklink "%homeDir%/.spacemacs"             "%dropbox%/.spacemacs"
