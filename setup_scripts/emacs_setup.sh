965e985614d2c23268c388cf11952edad979b639
Simon Ho <simonsays87@googlemail.com>
2017-05-05 06:06:58 -0700

    Condense config to single file

--- setup_scripts/emacs_setup.sh
+++ setup_scripts/emacs_setup.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 
 
-ln -s ~/Dropbox/dotfiles/emacs/.emacs.el ~/.emacs.el
-
-ln -s ~/Dropbox/dotfiles/emacs/.emacs_packages.el ~/.emacs_packages.el
-
-ln -s ~/Dropbox/dotfiles/emacs/.emacs_keybinds.el ~/.emacs_keybinds.el
+ln -s ~/Dropbox/dotfiles/emacs/init.el ~/.emacs.d/init.el
