86159397fb6d4d0cc29bd4c8704771a962dddb25
Simon Ho <simonho.ubc@gmail.com>
2023-02-06 23:47:40 -0800

    Clone dotfiles

--- setup_scripts/windows/winget_install.ps1
+++ setup_scripts/windows/winget_install.ps1
@@ -1,8 +1,14 @@
+######################################################
+# Install packages
+######################################################
+Write-Host "Installing packages"
+
 winget install -e --id 7zip.7zip
 winget install -e --id Adobe.Acrobat.Reader.64-bit
 winget install -e --id Anaconda.Anaconda3
 winget install -e --id Twilio.Authy
 winget install -e --id Bitwarden.Bitwarden
+winget install -e --id Google.Chrome
 winget install -e --id Discord.Discord
 winget install -e --id Docker.DockerDesktop
 winget install -e --id Google.Drive
@@ -11,8 +17,8 @@ winget install -e --id Mozilla.Firefox
 winget install -e --id GIMP.GIMP
 winget install -e --id Git.Git
 winget install -e --id Axosoft.GitKraken
-winget install -e --id Google.Chrome
 winget install -e --id TheDocumentFoundation.LibreOffice
+winget install -e --id Neovim.Neovim
 winget install -e --id OBSProject.OBSStudio
 winget install JanDeDobbeleer.OhMyPosh -s winget
 winget install -e --id Parsec.Parsec
@@ -28,8 +34,18 @@ winget install -e --id DigitalScholar.Zotero
 # Configure Git globals
 ######################################################
 Write-Host "Configuring Git globals"
+
 $userName = Read-Host 'Enter your name for git configuration'
 $userEmail = Read-Host 'Enter your email for git configuration'
 
-& 'C:\Program Files\Git\bin\git' config --global user.email $userEmail
-& 'C:\Program Files\Git\bin\git' config --global user.name $userName
+git config --global user.email $userEmail
+git config --global user.name $userName
+
+######################################################
+# Clone dotfiles repo and symlink them
+######################################################
+Write-Host "Cloning dotfiles repo"
+
+git clone https://github.com/sho-87/dotfiles.git $env:USERPROFILE\dotfiles
+
+New-Item -ItemType SymbolicLink -Path $env:USERPROFILE\AppData\Local\nvim -Target $env:USERPROFILE\dotfiles\nvim
