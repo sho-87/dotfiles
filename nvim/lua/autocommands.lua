6d3f3e6ccc2688afcc0c40d33c01be9eb5b17e68
Simon Ho <simonho.ubc@gmail.com>
2023-02-18 13:19:15 -0800

    Create augroup for bufferline

--- nvim/lua/autocommands.lua
+++ nvim/lua/autocommands.lua
@@ -1,21 +1,20 @@
--- Open minimap on file open
--- vim.api.nvim_create_autocmd({ 'BufNewFile', 'BufReadPost' }, {
---   command = 'lua MiniMap.open()'
--- })
-
 -- Offset bufferline when file tree is open
-vim.api.nvim_create_autocmd('BufWinEnter', {
-  callback = function(tbl)
-    if vim.bo[tbl.buf].filetype == 'neo-tree' then
-      require'bufferline.api'.set_offset(36, 'FileTree')
-    end
-  end
+local bufferlineGroup = vim.api.nvim_create_augroup("BufferlineOffset", { clear = true })
+
+vim.api.nvim_create_autocmd("BufWinEnter", {
+	callback = function(tbl)
+		if vim.bo[tbl.buf].filetype == "neo-tree" then
+			require("bufferline.api").set_offset(36, "FileTree")
+		end
+	end,
+	group = bufferlineGroup,
 })
 
-vim.api.nvim_create_autocmd({'BufWinLeave', 'BufWipeout'}, {
-  callback = function(tbl)
-    if vim.bo[tbl.buf].filetype == 'neo-tree' then
-      require'bufferline.api'.set_offset(0)
-    end
-  end
+vim.api.nvim_create_autocmd({ "BufWinLeave", "BufWipeout" }, {
+	callback = function(tbl)
+		if vim.bo[tbl.buf].filetype == "neo-tree" then
+			require("bufferline.api").set_offset(0)
+		end
+	end,
+	group = bufferlineGroup,
 })
