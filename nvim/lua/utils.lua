e2558e8f27f2595397cfe6176cbe7984cbeec73c
Simon Ho <simonho.ubc@gmail.com>
2023-02-18 20:25:36 -0800

    Add function utils

---
+++ nvim/lua/utils.lua
@@ -0,0 +1,13 @@
+local M = { }
+
+-- Functional wrapper for mapping custom keybindings
+local function map(mode, lhs, rhs, opts)
+	local options = { noremap = true }
+	if opts then
+		options = vim.tbl_extend("force", options, opts)
+	end
+	vim.api.nvim_set_keymap(mode, lhs, rhs, options)
+end
+M.map = map
+
+return M
