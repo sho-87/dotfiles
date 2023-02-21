eb006bf906a1d2b7aa7fd4f7f55cc74fd7455aea
Simon Ho <simonho.ubc@gmail.com>
2023-02-21 03:44:35 -0800

    Stop continuing comments on new lines

--- nvim/lua/autocommands.lua
+++ nvim/lua/autocommands.lua
@@ -16,3 +16,10 @@ vim.api.nvim_create_autocmd({ "BufWinLeave", "BufWipeout" }, {
 	end,
 	group = bufferlineGroup,
 })
+
+-- Stop newline comment continuation
+vim.api.nvim_create_autocmd("BufEnter", {
+	callback = function()
+		vim.opt.formatoptions = vim.opt.formatoptions - { "c", "r", "o" }
+	end,
+})
