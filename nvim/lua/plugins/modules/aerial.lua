a0c2a044a84b5e86bea436d993be5fe9ffd43db5
Simon Ho <sho-87@users.noreply.github.com>
2023-03-10 09:50:10 -0800

    Update aerial settings

--- nvim/lua/plugins/modules/aerial.lua
+++ nvim/lua/plugins/modules/aerial.lua
@@ -2,7 +2,6 @@ local M = {
 	"stevearc/aerial.nvim",
 	cond = vim.g.vscode == nil,
 	enabled = true,
-	cmd = { "AerialToggle", "AerialOpen", "AerialOpenAll" },
 }
 
 function M.config()
@@ -11,10 +10,15 @@ function M.config()
 			vim.keymap.set("n", "[a", "<cmd>AerialPrev<CR>", { buffer = bufnr })
 			vim.keymap.set("n", "]a", "<cmd>AerialNext<CR>", { buffer = bufnr })
 		end,
+		backends = { "lsp", "treesitter", "markdown", "man" },
 		layout = {
+			max_width = 40,
+			width = nil,
+			min_width = 20,
 			default_direction = "right",
 			preserve_equality = false,
 		},
+		lazy_load = true,
 		close_automatic_events = { "unsupported" },
 		highlight_on_hover = true,
 		show_guides = true,
