6ab52cbfe23ac5c424de5d0c78aee74d1aebba72
Simon Ho <simonho.ubc@gmail.com>
2023-03-13 07:57:39 -0700

    Update winget packages

--- setup_scripts/windows/winget_install.ps1
+++ setup_scripts/windows/winget_install.ps1
@@ -12,6 +12,7 @@ winget install -e --id Discord.Discord
 winget install -e --id Docker.DockerDesktop
 winget install -e --id Google.Drive
 winget install -e --id Dropbox.Dropbox
+winget install -e --id Gyan.FFmpeg
 winget install -e --id Mozilla.Firefox
 winget install -e --id GIMP.GIMP
 winget install -e --id Git.Git
@@ -19,14 +20,14 @@ winget install -e --id Axosoft.GitKraken
 winget install -e --id TheDocumentFoundation.LibreOffice
 winget install -e --id Neovim.Neovim
 winget install -e --id OBSProject.OBSStudio
-winget install JanDeDobbeleer.OhMyPosh -s winget
+winget install -e --id JanDeDobbeleer.OhMyPosh -s winget
 winget install -e --id Parsec.Parsec
-winget install -e --id Microsoft.PowerToys
 winget install -e --id Microsoft.PowerShell
+winget install -e --id Microsoft.PowerToys
 winget install -e --id Nvidia.GeForceExperience
 winget install -e --id RProject.R
-winget install wez.wezterm
 winget install -e --id Valve.Steam
+winget install -e --id wez.wezterm
 winget install -e --id Microsoft.VisualStudioCode
 winget install -e --id VideoLAN.VLC
 winget install -e --id DigitalScholar.Zotero
