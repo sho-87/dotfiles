d4f2b96673e6466ae3d39c96d77c1e7216e75f56
Simon Ho <simonho.ubc@gmail.com>
2023-03-14 18:46:46 -0700

    Check for directories before creating symlinks

--- setup_scripts/windows/set_config.ps1
+++ setup_scripts/windows/set_config.ps1
@@ -1,16 +1,21 @@
 #-----------------------------------------------------
 # Clone dotfiles repo and symlink them
 #-----------------------------------------------------
-# FIXME: check for existing file/directory first
-Write-Host "Cloning dotfiles repo..."
-git clone https://github.com/sho-87/dotfiles.git $env:USERPROFILE\dotfiles
+if (!(Test-Path $env:USERPROFILE\dotfiles)) {
+    Write-Host "Cloning dotfiles repo..."
+    git clone https://github.com/sho-87/dotfiles.git $env:USERPROFILE\dotfiles
+}
 
-Write-Host "Symlinking nvim config..."
-New-Item -ItemType SymbolicLink -Path $env:USERPROFILE\AppData\Local\nvim -Target $env:USERPROFILE\dotfiles\nvim
+if (!(Test-Path $env:USERPROFILE\AppData\Local\nvim)) {
+    Write-Host "Symlinking nvim config..."
+    New-Item -ItemType SymbolicLink -Path $env:USERPROFILE\AppData\Local\nvim -Target $env:USERPROFILE\dotfiles\nvim
+}
 
-Write-Host "Symlinking wezterm config..."
-New-Item -ItemType Directory -Path $env:USERPROFILE\.config
-New-Item -ItemType SymbolicLink -Path $env:USERPROFILE\.config\wezterm -Target $env:USERPROFILE\dotfiles\wezterm
+if (!(Test-Path $env:USERPROFILE\.config\wezterm)) {
+    Write-Host "Symlinking wezterm config..."
+    New-Item -ItemType Directory -Path $env:USERPROFILE\.config
+    New-Item -ItemType SymbolicLink -Path $env:USERPROFILE\.config\wezterm -Target $env:USERPROFILE\dotfiles\wezterm
+}
 
 #-----------------------------------------------------
 # Set default shell
@@ -20,10 +25,11 @@ New-Item -ItemType SymbolicLink -Path $env:USERPROFILE\.config\wezterm -Target $
 #-----------------------------------------------------
 # Configure Git globals
 #-----------------------------------------------------
-Write-Host "Configuring Git globals..."
 
-$userName = Read-Host 'Enter your name for git configuration'
-$userEmail = Read-Host 'Enter your email for git configuration'
-
-git config --global user.email $userEmail
-git config --global user.name $userName
+# Write-Host "Configuring Git globals..."
+#
+# $userName = Read-Host 'Enter your name for git configuration'
+# $userEmail = Read-Host 'Enter your email for git configuration'
+#
+# git config --global user.email $userEmail
+# git config --global user.name $userName
