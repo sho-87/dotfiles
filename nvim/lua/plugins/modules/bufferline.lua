7ebe2c13657f99af16d23372bca697b2d9dc41c0
Simon Ho <simonho.ubc@gmail.com>
2023-03-28 23:32:50 -0700

    Bring back close button

--- nvim/lua/plugins/modules/bufferline.lua
+++ nvim/lua/plugins/modules/bufferline.lua
@@ -34,7 +34,7 @@ function M.config()
 			diagnostics = false,
 			color_icons = true, -- whether or not to add the filetype icon highlights
 			show_buffer_icons = true, -- disable filetype icons for buffers
-			show_buffer_close_icons = false,
+			show_buffer_close_icons = true,
 			show_buffer_default_icon = true, -- whether or not an unrecognised filetype should show a default icon
 			show_close_icon = true,
 			show_tab_indicators = true,
