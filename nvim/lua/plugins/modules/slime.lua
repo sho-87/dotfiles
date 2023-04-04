3d676a4b71467d0f361e84762dd839e9e6bf93b1
Simon Ho <simonho.ubc@gmail.com>
2023-04-04 00:05:03 -0700

    Fix quarto delimiter

--- nvim/lua/plugins/modules/slime.lua
+++ nvim/lua/plugins/modules/slime.lua
@@ -10,7 +10,7 @@ function M.config()
 	vim.g.slime_target = "neovim"
 	vim.g.slime_dont_ask_default = 1
 	vim.g.markdown_fenced_languages = { "html", "python", "bash=sh", "R=r" }
-	vim.g.slime_cell_delimiter = "```"
+	vim.g.slime_cell_delimiter = [[^```{\?\a\+}\?$]]
 	vim.g.slime_python_ipython = 1
 	vim.g.slime_bracketed_paste = 1
 	vim.g.slime_no_mappings = 1
