436784d1b00eac1b7f38a7f221aa483455b23c31
Simon Ho <simonho.ubc@gmail.com>
2023-04-07 15:00:34 -0700

    Add copilot keybinds

--- nvim/lua/plugins/modules/github-copilot.lua
+++ nvim/lua/plugins/modules/github-copilot.lua
@@ -8,21 +8,6 @@ local M = {
 
 function M.config()
 	require("copilot").setup({
-		panel = {
-			enabled = true,
-			auto_refresh = false,
-			keymap = {
-				jump_prev = "[[",
-				jump_next = "]]",
-				accept = "<CR>",
-				refresh = "gr",
-				open = "<M-CR>",
-			},
-			layout = {
-				position = "bottom", -- | top | left | right
-				ratio = 0.4,
-			},
-		},
 		suggestion = {
 			enabled = true,
 			auto_trigger = true,
@@ -31,9 +16,9 @@ function M.config()
 				accept = "<c-l>",
 				accept_word = false,
 				accept_line = false,
-				dismiss = "<C-e>",
-				next = "<M-]>",
-				prev = "<M-[>",
+				dismiss = "<c-e>",
+				next = "<c-j>",
+				prev = "<c-k>",
 			},
 		},
 	})
