704909168ce1d0beee501264eb46c9d62816afd8
Simon Ho <simonho.ubc@gmail.com>
2023-04-08 22:07:08 -0700

    Restructure alpha config

---
+++ nvim/lua/quotes.lua
@@ -0,0 +1,10 @@
+local M = {}
+
+M.roar = {
+	[["if you have to wait for it to roar out of you,]],
+	[[then wait patiently.]],
+	[[if it never does roar out of you,]],
+	[[do something else."]],
+}
+
+return M
