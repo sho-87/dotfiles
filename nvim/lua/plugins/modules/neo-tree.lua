27b119468ffe8230f76040dd522414fee863c211
Simon Ho <simonho.ubc@gmail.com>
2023-04-09 01:32:19 -0700

    Keep focus on neo-tree after open

--- nvim/lua/plugins/modules/neo-tree.lua
+++ nvim/lua/plugins/modules/neo-tree.lua
@@ -57,6 +57,15 @@ function M.config()
 			},
 			mappings = {
 				["<space>"] = "none",
+				["<cr>"] = function(state)
+					local node = state.tree:get_node()
+					if require("neo-tree.utils").is_expandable(node) then
+						state.commands["toggle_node"](state)
+					else
+						state.commands["open"](state)
+						vim.cmd("Neotree reveal")
+					end
+				end,
 				["a"] = {
 					"add",
 					config = {
