f335d930f6a5745157072e9f1eee851ca375740b
Simon Ho <simonho.ubc@gmail.com>
2023-04-19 23:13:10 -0700

    Random stuff

--- nvim/lua/plugins/modules/codewindow.lua
+++ nvim/lua/plugins/modules/codewindow.lua
@@ -8,19 +8,19 @@ function M.config()
 	local codewindow = require("codewindow")
 	codewindow.setup({
 		active_in_terminals = false, -- Should the minimap activate for terminal buffers
-		auto_enable = true, -- Automatically open the minimap when entering a (non-excluded) buffer 
-		exclude_filetypes = { "alpha", "neo-tree", "aerial", "OverseeList" }, 
+		auto_enable = true, -- Automatically open the minimap when entering a (non-excluded) buffer
+		exclude_filetypes = { "alpha", "neo-tree", "aerial", "OverseeList", "Trouble" },
 		max_minimap_height = nil, -- The maximum height the minimap can take (including borders)
 		max_lines = nil, -- If auto_enable is true, don't open the minimap for buffers which have too many lines.
 		minimap_width = 10, -- The width of the text part of the minimap
-        use_treesitter = true, -- Use nvim-treesitter to highlight the code
+		use_treesitter = true, -- Use nvim-treesitter to highlight the code
 		use_lsp = true, -- Use the builtin LSP to show errors and warnings
 		use_git = true, -- Show small dots to indicate git additions and deletions
 		width_multiplier = 5, -- How many characters one dot represents
 		z_index = 1, -- The z-index the floating window will be on
 		show_cursor = true, -- Show the cursor position in the minimap
 		window_border = "single", -- The border style of the floating window (accepts all usual options)
-		events = { "TextChanged", "InsertLeave", "DiagnosticChanged", "FileWritePost" },
+		events = { "TextChanged", "InsertLeave", "DiagnosticChanged", "FileWritePost", "BufEnter" },
 	})
 end
 
