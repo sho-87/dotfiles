ce3fe29747c284810e2652f2f60f7a872a1ea2d3
Simon Ho <simonho.ubc@gmail.com>
2023-07-11 18:35:40 -0700

    Add package info plugin

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -40,6 +40,7 @@ map("n", "<leader>zi", "<cmd>Inspect<CR>", { desc = "TS Inspect" })
 map("n", "<leader>zI", "<cmd>InspectTree<CR>", { desc = "TS Inspect Tree" })
 map("n", "<leader>zpm", "<cmd>MarkdownPreview<cr>", { desc = "Markdown" })
 map("n", "<leader>zpq", "<cmd>!start quarto preview %:p<cr>", { desc = "Quarto" })
+map("n", "<leader>zn", "<cmd>Telescope package_info<cr>", { desc = "npm" })
 
 -- ╔═════════════════════════════════════════════════╗
 -- ║ Window / Splits                                 ║
---
+++ nvim/lua/plugins/modules/package-info.lua
@@ -0,0 +1,31 @@
+-- handle folds
+
+local M = {
+	"vuki656/package-info.nvim",
+	enabled = true,
+	dependencies = "MunifTanjim/nui.nvim",
+	ft = "json",
+}
+
+function M.config()
+	require("package-info").setup({
+		colors = {
+			up_to_date = "#3C4048", -- Text color for up to date dependency virtual text
+			outdated = "#d19a66", -- Text color for outdated dependency virtual text
+		},
+		icons = {
+			enable = true, -- Whether to display icons
+			style = {
+				up_to_date = "|  ", -- Icon for up to date dependencies
+				outdated = "|  ", -- Icon for outdated dependencies
+			},
+		},
+		autostart = true, -- Whether to autostart when `package.json` is opened
+		hide_up_to_date = true, -- It hides up to date versions when displaying virtual text
+		hide_unstable_versions = true, -- It hides unstable versions from version list e.g next-11.1.3-canary3
+		package_manager = "npm",
+	})
+	require("telescope").load_extension("package_info")
+end
+
+return M
