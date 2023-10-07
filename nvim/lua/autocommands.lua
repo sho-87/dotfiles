4a679b37f1a2d3e4fb325d2d650ace954937fb71
Simon Ho <simonho.ubc@gmail.com>
2023-10-07 03:18:04 -0700

    Hide tabline

--- nvim/lua/autocommands.lua
+++ nvim/lua/autocommands.lua
@@ -60,10 +60,10 @@ local alpha_group = vim.api.nvim_create_augroup("alpha", { clear = true })
 vim.api.nvim_create_autocmd("User", {
 	group = alpha_group,
 	pattern = "AlphaReady",
-	command = "set laststatus=0 | set showtabline=0",
+	command = "set laststatus=0",
 })
 vim.api.nvim_create_autocmd("User", {
 	group = alpha_group,
 	pattern = "AlphaClosed",
-	command = "set laststatus=3 | set showtabline=2",
+	command = "set laststatus=3",
 })
--- nvim/lua/settings.lua
+++ nvim/lua/settings.lua
@@ -2,6 +2,7 @@ vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.cursorline = true
 vim.opt.laststatus = 3
+vim.opt.showtabline = 0
 vim.opt.signcolumn = "yes:1"
 vim.opt.fillchars = "eob: "
 
