54c2f4f913b54dcdb86b790b96e3f3f4ac9da6d2
Simon Ho <simonho.ubc@gmail.com>
2023-12-10 23:36:46 -0800

    Simplify tabs

--- emacs/init.el
+++ emacs/init.el
@@ -419,6 +419,12 @@ beacon-blink-when-point-moves t)
 	:config
 	(global-evil-surround-mode 1))
 
+(use-package evil-collection
+:after evil
+:demand t
+:config
+(evil-collection-init))
+
 (use-package which-key
 	:demand t
 	:diminish
@@ -637,6 +643,72 @@ treemacs-fringe-indicator-mode 'always))
 						"t" 'centaur-tabs-forward
 						"T" 'centaur-tabs-backward))
 
+(defun centaur-tabs-buffer-groups ()
+"`centaur-tabs-buffer-groups' control buffers' group rules.
+
+Group centaur-tabs with mode if buffer is derived from `eshell-mode' `emacs-lisp-mode' `dired-mode' `org-mode' `magit-mode'.
+All buffer name start with * will group to \"Emacs\".
+Other buffer group by `centaur-tabs-get-group-name' with project name."
+(list
+(cond
+((or (string-equal "*" (substring (buffer-name) 0 1))
+(memq major-mode '(magit-process-mode
+magit-status-mode
+magit-diff-mode
+magit-log-mode
+magit-file-mode
+magit-blob-mode
+magit-blame-mode
+)))
+"Emacs")
+((derived-mode-p 'prog-mode)
+"Editing")
+((derived-mode-p 'dired-mode)
+"Dired")
+((memq major-mode '(helpful-mode
+help-mode))
+"Help")
+((memq major-mode '(org-mode
+org-agenda-clockreport-mode
+org-src-mode
+org-agenda-mode
+org-beamer-mode
+org-indent-mode
+org-bullets-mode
+org-cdlatex-mode
+org-agenda-log-mode
+diary-mode))
+"OrgMode")
+(t
+(centaur-tabs-get-group-name (current-buffer))))))
+
+(defun centaur-tabs-hide-tab (x)
+"Do no to show buffer X in tabs."
+(let ((name (format "%s" x)))
+(or
+;; Current window is not dedicated window.
+(window-dedicated-p (selected-window))
+
+;; Buffer name not match below blacklist.
+(string-prefix-p "*epc" name)
+(string-prefix-p "*helm" name)
+(string-prefix-p "*Helm" name)
+(string-prefix-p "*Compile-Log*" name)
+(string-prefix-p "*lsp" name)
+(string-prefix-p "*company" name)
+(string-prefix-p "*Flycheck" name)
+(string-prefix-p "*tramp" name)
+(string-prefix-p " *Mini" name)
+(string-prefix-p "*help" name)
+(string-prefix-p "*straight" name)
+(string-prefix-p " *temp" name)
+(string-prefix-p "*Help" name)
+
+;; Is not magit buffer.
+(and (string-prefix-p "magit" name)
+(not (file-name-extension name)))
+)))
+
 (use-package format-all
 	:diminish
 	:commands format-all-mode
@@ -743,11 +815,11 @@ treemacs-fringe-indicator-mode 'always))
 (use-package lsp-ui
 	:commands lsp-ui-mode)
 
-(use-package lsp-treemacs
-	:after '(lsp-mode treemacs)
-	:init
-	(lsp-treemacs-sync-mode 1)
-	:commands lsp-treemacs-errors-list)
+;; (use-package lsp-treemacs
+;; 	:after '(lsp-mode treemacs)
+;; 	:init
+;; 	(lsp-treemacs-sync-mode 1)
+;; 	:commands lsp-treemacs-errors-list)
 
 (use-package flycheck
 	:diminish
@@ -854,6 +926,14 @@ treemacs-fringe-indicator-mode 'always))
 
 (use-package terraform-mode
 :custom (terraform-format-on-save t))
+
+(use-package graphql-ts-mode
+  :demand t
+  :mode ("\\.graphql\\'" "\\.gql\\'")
+  :config
+  (with-eval-after-load 'treesit
+    (add-to-list 'treesit-language-source-alist
+                 '(graphql "https://github.com/bkegley/tree-sitter-graphql"))))
 
 (use-package org
 	:elpaca nil
