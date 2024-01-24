fa14c870c3037eb62c4c6c7bcb88d65444062e8b
Simon <simonho.ubc@gmail.com>
2024-01-24 13:30:51 -0800

    Dired bind to find file in other window

--- emacs/init.el
+++ emacs/init.el
@@ -695,7 +695,7 @@ beacon-blink-when-point-moves t)
 	(dirvish-side-follow-mode)
 	(evil-define-key 'normal dirvish-mode-map
 			(kbd "<mouse-1>") 'dirvish-subtree-toggle-or-open
-			(kbd "<mouse-3>") 'dired-mouse-find-file
+			(kbd "<mouse-3>") 'dired-mouse-find-file-other-window
 			(kbd "q")					'dirvish-quit
 			(kbd "TAB")				'dirvish-subtree-toggle
 			(kbd "<return>")  'dired-find-file
