55a91bc8757f1ca0aeceb9b6586bd5852d3e01b5
Simon <simonho.ubc@gmail.com>
2024-02-12 00:48:52 -0800

    Wezterm config

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -4,17 +4,17 @@ local keybinds = require("keybinds")
 wezterm.on("gui-startup", function(cmd)
 	local _, _, window = wezterm.mux.spawn_window(cmd or {})
 	-- window:gui_window():maximize()
-	window:gui_window():set_inner_size(850, 500)
+	window:gui_window():set_inner_size(1080, 600)
 end)
 
 local is_windows = function()
-	return wezterm.target_triple:find("windows") ~= nil
+	return wezterm.target_triple == 'x86_64-pc-windows-msvc'
 end
 
 local default_prog
 local launch_menu
 
-if(is_windows == true) then
+if(is_windows) then
 	default_prog = { "nu.exe" }
 	launch_menu = {
 		{
@@ -46,42 +46,44 @@ else
 end
 
 return {
-	show_update_window = true,
-	enable_wayland = false,
-	unicode_version = 14,
+	adjust_window_size_when_changing_font_size = false,
+	animation_fps = 60,
+	automatically_reload_config = true,
 	color_scheme_dirs = { "~/.config/wezterm/colors" },
 	color_scheme = "kanagawa",
+ 	default_cursor_style = "SteadyBar",
+	disable_default_key_bindings = true,
+	enable_scroll_bar = true,
+	enable_wayland = true,
 	font_size = 11,
 	font = wezterm.font_with_fallback({
 		{ family = "FiraCode Nerd Font", weight = "Regular" },
 		"Source_Code_Pro",
 		"JetBrains Mono",
 	}),
-	harfbuzz_features = { "cv01", "cv02", "ss03", "ss05", "ss07", "ss08" },
-	default_cursor_style = "SteadyBar",
-	-- force_reverse_video_cursor = true,
-	underline_position = -2,
 	front_end = "WebGpu",
+	harfbuzz_features = { "cv01", "cv02", "ss03", "ss05", "ss07", "ss08" },
+	hide_tab_bar_if_only_one_tab = false,
+	inactive_pane_hsb = {
+		saturation = 0.9,
+		brightness = 0.7,
+	},
+	show_update_window = true,
+	underline_position = -2,
+	unicode_version = 14,
+	use_fancy_tab_bar = true,
+	use_resize_increments = true,
 	webgpu_power_preference = "HighPerformance",
-	animation_fps = 60,
-	adjust_window_size_when_changing_font_size = false,
+	window_background_opacity = 0.95,
+	window_close_confirmation = "NeverPrompt",
+	window_decorations = "TITLE|RESIZE",
 	window_padding = {
 		left = 0,
 		right = 0,
 		top = 0,
 		bottom = 0,
 	},
-	window_background_opacity = 0.95,
-	window_decorations = "TITLE|RESIZE",
-	use_resize_increments = true,
-	inactive_pane_hsb = {
-		saturation = 0.9,
-		brightness = 0.7,
-	},
-	hide_tab_bar_if_only_one_tab = false,
-	use_fancy_tab_bar = true,
-	disable_default_key_bindings = true,
-	leader = { key = "Space", mods = "CTRL", timeout_milliseconds = 1000 },
+	leader = { key = "Space", mods = "CTRL", timeout_milliseconds = 1500 },
 	keys = keybinds.basic_binds,
 	key_tables = keybinds.key_tables,
 	default_prog = default_prog,
