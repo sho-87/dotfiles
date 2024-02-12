2c8763076f075d7af4d8da49be5d009f9540cb82
Simon <simonho.ubc@gmail.com>
2024-02-12 00:48:39 -0800

    Make wezterm keybinds more like emacs

--- .config/wezterm/keybinds.lua
+++ .config/wezterm/keybinds.lua
@@ -16,8 +16,10 @@ M.basic_binds = {
     end),
 	},
 	{ key = "v", mods = "CTRL", action = act.PasteFrom("Clipboard") },
-	{ key = "p", mods = "CTRL|SHIFT", action = act.ActivateCommandPalette },
-	{ key = "n", mods = "LEADER", action = act.SpawnWindow },
+	{ key = "p", mods = "CTRL", action = act.ActivateCommandPalette },
+	{ key = "v", mods = "LEADER", action = act.ActivateCopyMode },
+	{ key = 'q', mods = 'LEADER', action = act.QuitApplication },
+	{ key = "j", mods = "LEADER", action = act.ShowTabNavigator },
 	{ key = "w", mods = "LEADER", action = act.ActivateKeyTable({ name = "manage_pane" }) },
 	{ key = "r", mods = "LEADER", action = act.ActivateKeyTable({ name = "resize_pane", one_shot = false }) },
 }
@@ -25,15 +27,14 @@ M.basic_binds = {
 M.key_tables = {
 	manage_pane = {
 		{ key = "w", action = act.PaneSelect },
-		{ key = "q", action = act.CloseCurrentPane({ confirm = true }) },
+		{ key = "n", action = act.SpawnWindow },
+		{ key = "d", action = act.CloseCurrentPane({ confirm = true }) },
 		{ key = "h", action = act.ActivatePaneDirection("Left") },
 		{ key = "l", action = act.ActivatePaneDirection("Right") },
 		{ key = "k", action = act.ActivatePaneDirection("Up") },
 		{ key = "j", action = act.ActivatePaneDirection("Down") },
-		{ key = "H", action = act.SplitPane({ direction = "Left" }) },
-		{ key = "L", action = act.SplitPane({ direction = "Right" }) },
-		{ key = "K", action = act.SplitPane({ direction = "Up" }) },
-		{ key = "J", action = act.SplitPane({ direction = "Down" }) },
+		{ key = "v", action = act.SplitPane({ direction = "Right" }) },
+		{ key = "s", action = act.SplitPane({ direction = "Down" }) },
 	},
 	resize_pane = {
 		{ key = "LeftArrow", action = act.AdjustPaneSize({ "Left", 1 }) },
