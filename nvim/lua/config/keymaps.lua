64f8b6007d8659693ba7a21789d9ab49a4fc6e2c
Simon <simonho.ubc@gmail.com>
2024-03-04 01:51:09 -0800

    feat(nvim): add neovide settings

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,14 +1,15 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
 
 -- Delete some default keymaps
 vim.api.nvim_del_keymap("n", "<leader>L")
 vim.api.nvim_del_keymap("n", "<leader>-")
 vim.api.nvim_del_keymap("n", "<leader>|")
+vim.api.nvim_del_keymap("n", "<leader>w|")
 
 -- Custom keymaps
 vim.keymap.set("i", "<C-H>", "<C-W>", { desc = "Delete word backward" }) -- Delete word backwards; C-H = C-BS
 vim.keymap.set("i", "<C-Del>", "<C-o>dw", { desc = "Delete word forward" }) -- Delete word forwards
 vim.keymap.set({ "n", "x" }, "gg", "mggg")
 vim.keymap.set({ "n", "x" }, "G", "mgG")
+vim.keymap.set("n", "<leader>w\\", "<C-W>v", { desc = "Split window right" })
--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,3 +1,10 @@
--- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
--- Add any additional options here
+
+vim.o.guifont = "FiraCode Nerd Font:h12"
+
+if vim.g.neovide then
+  vim.g.neovide_transparency = 0.98
+  vim.keymap.set("v", "<C-c>", '"+y') -- Copy
+  vim.keymap.set("n", "<C-v>", '"+P') -- Paste normal mode
+  vim.keymap.set("v", "<C-v>", '"+P') -- Paste visual mode
+end
