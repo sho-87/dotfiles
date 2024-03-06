226dd5cf94772a811611b4d99350b82cec79c242
Simon <simonho.ubc@gmail.com>
2024-03-05 20:42:00 -0800

    feat(nvim): add keymap for closing other windows

--- nvim/lua/plugins/mini.lua
+++ nvim/lua/plugins/mini.lua
@@ -1,6 +1,19 @@
 return {
-  "echasnovski/mini.bufremove",
-  keys = {
-    { "<leader>bD", vim.NIL },
+  {
+    "echasnovski/mini.bufremove",
+    keys = {
+      { "<leader>bD", vim.NIL },
+    },
+  },
+  {
+    "echasnovski/mini.indentscope",
+    draw = {
+      delay = 50,
+      animation = require("mini.indentscope").gen_animation.quadratic({
+        easing = "out",
+        duration = 100,
+        unit = "total",
+      }),
+    },
   },
 }
