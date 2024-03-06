300cfcb12e17b62065f34453e6625d9797734305
Simon Ho <simonho.ubc@gmail.com>
2024-03-06 09:36:35 -0800

    fix(nvim): fix conflicting keybind for aerial
    conflicts with lsp organize imports

--- nvim/lua/plugins/aerial.lua
+++ nvim/lua/plugins/aerial.lua
@@ -2,7 +2,6 @@ return {
   "stevearc/aerial.nvim",
   cmd = { "AerialToggle", "AerialOpen", "AerialOpenAll" },
   keys = {
-    { "<leader>cs", vim.NIL },
-    { "<leader>co", "<cmd>AerialToggle<cr>", desc = "Aerial (Symbols)" },
+    { "<leader>cs", "<cmd>AerialToggle<cr>", desc = "Aerial (Symbols)" },
   },
 }
