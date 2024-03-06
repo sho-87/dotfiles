36718c5b2350a5dd164deb6232a3305561f5f6cd
Simon Ho <simonho.ubc@gmail.com>
2024-03-05 21:27:50 -0800

    feat(nvim): add insert mode paste map for neovide

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -26,3 +26,10 @@ vim.keymap.set("n", "<leader>zm", "<cmd>Mason<cr>", { desc = "Mason" })
 vim.keymap.set("n", "<leader>`", function()
   require("lazyvim.util").terminal({ "nu" }, { esc_esc = true, ctrl_hjkl = true })
 end, { desc = "Terminal" })
+
+if vim.g.neovide then
+  vim.keymap.set("v", "<C-c>", '"+y') -- Copy
+  vim.keymap.set("n", "<C-v>", '"+P') -- Paste normal mode
+  vim.keymap.set("v", "<C-v>", '"+P') -- Paste visual mode
+  vim.keymap.set("i", "<C-v>", "<c-r>+") -- Paste insert mode
+end
