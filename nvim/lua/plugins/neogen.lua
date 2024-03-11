83818f1de0a5f2b80a85d79605bd5b72f35927fe
Simon Ho <simonho.ubc@gmail.com>
2024-03-11 02:03:13 -0700

    feat(nvim): add documentation generation with neogen

---
+++ nvim/lua/plugins/neogen.lua
@@ -0,0 +1,29 @@
+return {
+  "danymat/neogen",
+  event = "LazyFile",
+  config = true,
+  version = "*",
+  keys = {
+    { "<leader>cD", "<cmd>lua require('neogen').generate()<CR>", desc = "Generate docs" },
+  },
+  opts = {
+    snippet_engine = "luasnip",
+    languages = {
+      python = {
+        template = {
+          annotation_convention = "google_docstrings",
+        },
+      },
+      typescript = {
+        template = {
+          annotation_convention = "tsdoc",
+        },
+      },
+      typescriptreact = {
+        template = {
+          annotation_convention = "tsdoc",
+        },
+      },
+    },
+  },
+}
