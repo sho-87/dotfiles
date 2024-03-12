4f79c94fa976c6ca3ec43bc284471230264c134f
Simon Ho <simonho.ubc@gmail.com>
2024-03-12 00:56:44 -0700

    feat(nvim): remove spectre

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -4,9 +4,6 @@ vim.g.maplocalleader = ","
 
 vim.opt.list = false
 vim.opt.cursorline = true
-vim.o.foldcolumn = "1"
-vim.o.foldlevel = 99
-vim.o.foldlevelstart = 99
 vim.o.foldenable = true
 vim.lsp.set_log_level("off")
 
--- nvim/lua/plugins/disabled.lua
+++ nvim/lua/plugins/disabled.lua
@@ -2,4 +2,5 @@ return {
   { "zbirenbaum/copilot-cmp", enabled = false },
   { "echasnovski/mini.pairs", enabled = false },
   { "nvim-treesitter/nvim-treesitter-context", enabled = false },
+  { "nvim-pack/nvim-spectre", enabled = false },
 }
