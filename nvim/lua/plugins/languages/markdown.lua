2c49e410f75ab32cf1d5d64cd0315d6b88b1dd48
Simon Ho <simonho.ubc@gmail.com>
2024-03-17 06:26:37 -0700

    fix(nvim): local leader bind for markdown preview

---
+++ nvim/lua/plugins/languages/markdown.lua
@@ -0,0 +1,7 @@
+return {
+  "iamcco/markdown-preview.nvim",
+  keys = {
+    { "<leader>cp", ft = "markdown", vim.NIL },
+    { "<localleader>p", ft = "markdown", "<cmd>MarkdownPreviewToggle<cr>", desc = "Markdown Preview" },
+  },
+}
