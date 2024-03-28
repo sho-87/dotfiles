f75318de5256b89eb60452fffc80681bf5c5e417
Simon Ho <simonho.ubc@gmail.com>
2024-03-27 17:18:41 -0700

    feat(wezterm): window split binds for wezterm

--- .config/wezterm/keybinds.lua
+++ .config/wezterm/keybinds.lua
@@ -4,21 +4,22 @@ local M = {}
 
 M.basic_binds = {
 	{
-    key = 'c', mods = 'CTRL',
-    action = wezterm.action_callback(function(window, pane)
-        selection_text = window:get_selection_text_for_pane(pane)
-        is_selection_active = string.len(selection_text) ~= 0
-        if is_selection_active then
-            window:perform_action(act.CopyTo('Clipboard'), pane)
-        else
-            window:perform_action(act.SendKey{ key='c', mods='CTRL' }, pane)
-        end
-    end),
+		key = "c",
+		mods = "CTRL",
+		action = wezterm.action_callback(function(window, pane)
+			selection_text = window:get_selection_text_for_pane(pane)
+			is_selection_active = string.len(selection_text) ~= 0
+			if is_selection_active then
+				window:perform_action(act.CopyTo("Clipboard"), pane)
+			else
+				window:perform_action(act.SendKey({ key = "c", mods = "CTRL" }), pane)
+			end
+		end),
 	},
 	{ key = "v", mods = "CTRL", action = act.PasteFrom("Clipboard") },
 	{ key = "p", mods = "CTRL", action = act.ActivateCommandPalette },
 	{ key = "v", mods = "LEADER", action = act.ActivateCopyMode },
-	{ key = 'q', mods = 'LEADER', action = act.QuitApplication },
+	{ key = "q", mods = "LEADER", action = act.QuitApplication },
 	{ key = "j", mods = "LEADER", action = act.ShowTabNavigator },
 	{ key = "w", mods = "LEADER", action = act.ActivateKeyTable({ name = "manage_pane" }) },
 	{ key = "r", mods = "LEADER", action = act.ActivateKeyTable({ name = "resize_pane", one_shot = false }) },
@@ -33,8 +34,8 @@ M.key_tables = {
 		{ key = "l", action = act.ActivatePaneDirection("Right") },
 		{ key = "k", action = act.ActivatePaneDirection("Up") },
 		{ key = "j", action = act.ActivatePaneDirection("Down") },
-		{ key = "v", action = act.SplitPane({ direction = "Right" }) },
-		{ key = "s", action = act.SplitPane({ direction = "Down" }) },
+		{ key = "\\", action = act.SplitPane({ direction = "Right" }) },
+		{ key = "-", action = act.SplitPane({ direction = "Down" }) },
 	},
 	resize_pane = {
 		{ key = "LeftArrow", action = act.AdjustPaneSize({ "Left", 1 }) },
