0b1c5bfabc7f99aba1122aa85e966245ff25842e
Simon Ho <simonho.ubc@gmail.com>
2024-03-29 01:56:07 -0700

    feat(nvim): add more ignore characters to autopairs

--- nvim/lua/plugins/nvim-autopairs.lua
+++ nvim/lua/plugins/nvim-autopairs.lua
@@ -5,7 +5,7 @@ return {
   opts = {
     disable_filetype = { "TelescopePrompt", "spectre_panel" },
     check_ts = true,
-    ignored_next_char = [=[[%w%%%'%[%"%.%`%$]]=],
+    ignored_next_char = [=[[%w%%%'%[%"%.%`%$%{%(%<]]=],
     enable_check_bracket_line = true,
   },
 }
