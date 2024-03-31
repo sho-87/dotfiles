fc5c390a5dbd2ca9cd27f1258be2f1e79d308532
Simon Ho <simonho.ubc@gmail.com>
2024-03-31 03:13:31 -0700

    feat(nvim): add telescope prompt and delete keymap

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -41,6 +41,7 @@ local M = {
       scroll_strategy = "limit",
       wrap_results = false,
       dynamic_preview_title = true,
+      prompt_prefix = "  ",
       path_display = { "smart" },
       file_ignore_patterns = {
         "node_modules/",
@@ -56,6 +57,9 @@ local M = {
         i = {
           ["<C-j>"] = require("telescope.actions").move_selection_next,
           ["<C-k>"] = require("telescope.actions").move_selection_previous,
+          ["<C-H>"] = function()
+            vim.api.nvim_input("<C-w>")
+          end,
           ["<c-q>"] = require("trouble.providers.telescope").open_selected_with_trouble,
         },
         n = {
