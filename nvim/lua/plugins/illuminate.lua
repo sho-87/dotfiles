6bf2d7087f988fabbaa7d5f65229ff09ab5a9582
Simon Ho <simonho.ubc@gmail.com>
2024-03-31 01:17:18 -0700

    fix(nvim): dont illuminate in certain buffers

---
+++ nvim/lua/plugins/illuminate.lua
@@ -0,0 +1,12 @@
+return {
+  "RRethy/vim-illuminate",
+  opts = {
+    delay = 100,
+    under_cursor = false,
+    filetypes_denylist = {
+      "neo-tree",
+      "Trouble",
+      "spectre_panel",
+    },
+  },
+}
