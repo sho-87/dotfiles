52763bdbd34715e8b3639e851e3fba792396311c
Simon Ho <simonho.ubc@gmail.com>
2024-04-14 05:58:23 -0700

    fix(nvim): fix indent scope

--- nvim/lua/plugins/indents.lua
+++ nvim/lua/plugins/indents.lua
@@ -7,7 +7,7 @@ return {
       chunk = {
         enable = true,
         notify = true,
-        use_treesitter = true,
+        use_treesitter = false,
         chars = {
           horizontal_line = "─",
           vertical_line = "│",
@@ -26,7 +26,7 @@ return {
       },
       indent = {
         enable = false,
-        use_treesitter = true,
+        use_treesitter = false,
         chars = {
           "│",
         },
