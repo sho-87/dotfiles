695fba07d7d2686040006758e0c9ab230ca79155
Simon Ho <simonho.ubc@gmail.com>
2024-04-14 03:45:43 -0700

    fix(nvim): default layout for merge view

--- nvim/lua/plugins/git.lua
+++ nvim/lua/plugins/git.lua
@@ -6,4 +6,24 @@ return {
     { "<leader>gd", "<cmd>DiffviewOpen<cr>", desc = "Diffview" },
     { "<leader>gf", "<cmd>DiffviewFileHistory %<cr>", desc = "File History" },
   },
+  opts = {
+    view = {
+      default = {
+        -- Config for changed files, and staged files in diff views.
+        layout = "diff2_horizontal",
+        winbar_info = true,
+      },
+      merge_tool = {
+        -- Config for conflicted files in diff views during a merge or rebase.
+        layout = "diff3_mixed",
+        disable_diagnostics = true,
+        winbar_info = true,
+      },
+      file_history = {
+        -- Config for changed files in file history views.
+        layout = "diff2_horizontal",
+        winbar_info = true,
+      },
+    },
+  },
 }
