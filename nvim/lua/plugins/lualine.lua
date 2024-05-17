7600085b836b80684dbf7014e452930aff433451
Simon Ho <simonho.ubc@gmail.com>
2024-05-17 01:56:35 -0700

    feat(nvim): add filepath to lualine

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -63,7 +63,18 @@ local M = {
           },
         },
         lualine_c = {
-          Util.lualine.root_dir({ cwd = true }),
+          { "filetype", icon_only = true, separator = { right = "" }, padding = { left = 1, right = 0 } },
+          {
+            "filename",
+            file_status = true, -- Displays file status (readonly status, modified status)
+            shorting_target = 60, -- Shortens path to leave 40 spaces in the window
+            path = 1, -- 0: Just the filename
+            -- 1: Relative path
+            -- 2: Absolute path
+            -- 3: Absolute path, with tilde as the home directory
+            -- 4: Filename and parent dir, with tilde as the home directory
+            padding = { left = 0, right = 1 },
+          },
         },
         lualine_x = {
           -- stylua: ignore
