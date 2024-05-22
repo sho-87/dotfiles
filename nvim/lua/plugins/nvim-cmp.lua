7c62ffa0e0771c5797e130ebceaaf186e37cd747
Simon Ho <simonho.ubc@gmail.com>
2024-05-22 06:40:49 -0700

    feat(nvim): replace luasnip with builtin nvim snippets

--- nvim/lua/plugins/nvim-cmp.lua
+++ nvim/lua/plugins/nvim-cmp.lua
@@ -1,15 +1,5 @@
 return {
-  { -- disable default tab behaviour
-    "L3MON4D3/LuaSnip",
-    config = function(_, opts)
-      require("cmp").setup(opts)
-      require("luasnip.loaders.from_lua").lazy_load({ paths = vim.fn.stdpath("config") .. "/lua/luasnippets" })
-    end,
-    keys = function()
-      return {}
-    end,
-  },
-  { -- setup supertab
+  {
     "hrsh7th/nvim-cmp",
     opts = function(_, opts)
       local has_words_before = function()
@@ -18,7 +8,6 @@ return {
         return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
       end
 
-      local luasnip = require("luasnip")
       local cmp = require("cmp")
 
       opts.window = {
@@ -29,12 +18,11 @@ return {
       opts.mapping = vim.tbl_extend("force", opts.mapping, {
         ["<Tab>"] = cmp.mapping(function(fallback)
           if cmp.visible() then
-            -- You could replace select_next_item() with confirm({ select = true }) to get VS Code autocompletion behavior
             cmp.select_next_item()
-          -- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable()
-          -- this way you will only jump inside the snippet region
-          elseif luasnip.expand_or_locally_jumpable() then
-            luasnip.expand_or_jump()
+          elseif vim.snippet.active({ direction = 1 }) then
+            vim.schedule(function()
+              vim.snippet.jump(1)
+            end)
           elseif has_words_before() then
             cmp.complete()
           else
@@ -44,8 +32,10 @@ return {
         ["<S-Tab>"] = cmp.mapping(function(fallback)
           if cmp.visible() then
             cmp.select_prev_item()
-          elseif luasnip.jumpable(-1) then
-            luasnip.jump(-1)
+          elseif vim.snippet.active({ direction = -1 }) then
+            vim.schedule(function()
+              vim.snippet.jump(-1)
+            end)
           else
             fallback()
           end
