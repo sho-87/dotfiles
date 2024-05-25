c606dc1a6095dc9c7391f5b32a9bf9a1127a7509
Simon Ho <simonho.ubc@gmail.com>
2024-05-25 02:52:02 -0700

    fix(nvim): fix completion ghost text collisions

--- nvim/lua/plugins/nvim-cmp.lua
+++ nvim/lua/plugins/nvim-cmp.lua
@@ -15,23 +15,25 @@ return {
         documentation = cmp.config.window.bordered(),
       }
 
+      opts.experimental.ghost_text = false
+
       opts.mapping = vim.tbl_extend("force", opts.mapping, {
         ["<Tab>"] = cmp.mapping(function(fallback)
           if cmp.visible() then
-            cmp.select_next_item()
+            cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
           elseif vim.snippet.active({ direction = 1 }) then
             vim.schedule(function()
               vim.snippet.jump(1)
             end)
           elseif has_words_before() then
-            cmp.complete()
+            cmp.complete({ behavior = cmp.ConfirmBehavior.Replace })
           else
             fallback()
           end
         end, { "i", "s" }),
         ["<S-Tab>"] = cmp.mapping(function(fallback)
           if cmp.visible() then
-            cmp.select_prev_item()
+            cmp.select_prev_item({ behavior = cmp.SelectBehavior.Select })
           elseif vim.snippet.active({ direction = -1 }) then
             vim.schedule(function()
               vim.snippet.jump(-1)
