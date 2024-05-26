6ad8a23f84175811866c0ffd81a1224145b73d87
Simon Ho <simonho.ubc@gmail.com>
2024-05-26 10:37:49 -0700

    chore(nvim): formatting

--- nvim/lua/plugins/buffers.lua
+++ nvim/lua/plugins/buffers.lua
@@ -23,10 +23,12 @@ return {
     },
     opts = {
       options = {
-      -- stylua: ignore
-      close_command = function(n) require("mini.bufremove").delete(n, false) end,
-      -- stylua: ignore
-      right_mouse_command = function(n) require("mini.bufremove").delete(n, false) end,
+        close_command = function(n)
+          require("mini.bufremove").delete(n, false)
+        end,
+        right_mouse_command = function(n)
+          require("mini.bufremove").delete(n, false)
+        end,
         mode = "buffers",
         numbers = "none",
         indicator = {
