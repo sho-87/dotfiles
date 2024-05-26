f42f9d6b785f9d26bf108fba70e413848b8ae92c
Simon Ho <simonho.ubc@gmail.com>
2024-05-26 03:26:56 -0700

    feat(nvim): replace textobject move with lazy defaults

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -46,22 +46,6 @@ local M = {
     incremental_selection = {
       enable = false, -- use flash treesitter mode instead
     },
-    textobjects = {
-      move = {
-        enable = true,
-        set_jumps = true, -- whether to set jumps in the jumplist
-        goto_previous_start = {
-          ["[f"] = { query = "@function.outer", desc = "Previous function" },
-          ["[c"] = { query = "@class.outer", desc = "Previous class" },
-          ["[p"] = { query = "@parameter.inner", desc = "Previous parameter" },
-        },
-        goto_next_start = {
-          ["]f"] = { query = "@function.outer", desc = "Next function" },
-          ["]c"] = { query = "@class.outer", desc = "Next class" },
-          ["]p"] = { query = "@parameter.inner", desc = "Next parameter" },
-        },
-      },
-    },
   },
 }
 
