4ccbbc57af55dc2ce96fca32837d805e2d369588
Simon Ho <simonho.ubc@gmail.com>
2024-05-26 03:24:46 -0700

    feat(nvim): remove TS textobjects
    use mini AI instead

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -47,32 +47,6 @@ local M = {
       enable = false, -- use flash treesitter mode instead
     },
     textobjects = {
-      select = {
-        enable = true,
-        lookahead = true,
-        keymaps = {
-          -- You can use the capture groups defined in textobjects.scm
-          ["af"] = { query = "@function.outer", desc = "around a function" },
-          ["if"] = { query = "@function.inner", desc = "inner part of a function" },
-          ["ac"] = { query = "@class.outer", desc = "around a class" },
-          ["ic"] = { query = "@class.inner", desc = "inner part of a class" },
-          ["ai"] = { query = "@conditional.outer", desc = "around an if statement" },
-          ["ii"] = { query = "@conditional.inner", desc = "inner part of an if statement" },
-          ["al"] = { query = "@loop.outer", desc = "around a loop" },
-          ["il"] = { query = "@loop.inner", desc = "inner part of a loop" },
-          ["ap"] = { query = "@parameter.outer", desc = "around parameter" },
-          ["ip"] = { query = "@parameter.inner", desc = "inside a parameter" },
-        },
-        selection_modes = {
-          ["@parameter.outer"] = "v", -- charwise
-          ["@parameter.inner"] = "v", -- charwise
-          ["@function.outer"] = "v", -- charwise
-          ["@conditional.outer"] = "V", -- linewise
-          ["@loop.outer"] = "V", -- linewise
-          ["@class.outer"] = "<c-v>", -- blockwise
-        },
-        include_surrounding_whitespace = false,
-      },
       move = {
         enable = true,
         set_jumps = true, -- whether to set jumps in the jumplist
