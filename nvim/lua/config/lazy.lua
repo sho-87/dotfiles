bb333a00752c33ac37281621b3833ee65e120f2e
Simon Ho <simon@sprungstudios.com>
2024-06-04 09:56:41 -0700

    feat(nvim): add dial

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -12,6 +12,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.vscode" },
     { import = "lazyvim.plugins.extras.coding.mini-surround" },
     { import = "lazyvim.plugins.extras.coding.yanky" },
+    { import = "lazyvim.plugins.extras.editor.dial" },
     { import = "lazyvim.plugins.extras.lang.docker" },
     { import = "lazyvim.plugins.extras.lang.go" },
     { import = "lazyvim.plugins.extras.lang.json" },
