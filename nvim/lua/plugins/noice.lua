aceca1239868ea96789a341d063aee140edd3b99
Simon Ho <simonho.ubc@gmail.com>
2024-06-03 23:46:30 -0700

    feat(nvim): add noice config for presets

--- nvim/lua/plugins/noice.lua
+++ nvim/lua/plugins/noice.lua
@@ -1,19 +1,27 @@
+local utils = require("config/utils")
+
 return {
   "folke/noice.nvim",
   opts = {
+    presets = {
+      bottom_search = false, -- use a classic bottom cmdline for search
+      command_palette = true, -- position the cmdline and popupmenu together
+      long_message_to_split = true, -- long messages will be sent to a split
+      lsp_doc_border = true, -- add a border to hover docs and signature help
+    },
     lsp = {
-      progress = {
-        override = {
-          -- override the default lsp markdown formatter with Noice
-          ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-          -- override the lsp markdown formatter with Noice
-          ["vim.lsp.util.stylize_markdown"] = true,
-          -- override cmp documentation with Noice (needs the other options to work)
-          ["cmp.entry.get_documentation"] = true,
-        },
+      override = {
+        ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+        ["vim.lsp.util.stylize_markdown"] = true,
+        ["cmp.entry.get_documentation"] = true,
       },
     },
     views = {
+      hover = {
+        border = {
+          style = utils.border_chars_outer_thin,
+        },
+      },
       mini = {
         win_options = {
           winblend = 100,
