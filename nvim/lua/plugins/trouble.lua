2e397393b23b4111df56b10fccba51a684530a1d
Simon Ho <simonho.ubc@gmail.com>
2024-06-08 07:22:21 -0700

    feat(nvim): add config for trouble

---
+++ nvim/lua/plugins/trouble.lua
@@ -0,0 +1,14 @@
+return {
+  "folke/trouble.nvim",
+  opts = {
+    focus = true, -- Focus the window when opened
+    follow = true, -- Follow the current item
+    indent_guides = true, -- show indent guides
+    modes = {
+      symbols = {
+        focus = true,
+        win = { position = "right" },
+      },
+    },
+  },
+}
