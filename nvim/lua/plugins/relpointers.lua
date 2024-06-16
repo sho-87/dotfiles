62dbcf9900fb6e5c0872a94f880394172795a35e
Simon Ho <simonho.ubc@gmail.com>
2024-06-16 00:31:42 -0700

    feat(nvim): test relpointers plugin

---
+++ nvim/lua/plugins/relpointers.lua
@@ -0,0 +1,20 @@
+return {
+  "scheisa/relpointers.nvim",
+  event = "LazyFile",
+  opts = {
+    amount = 2,
+    distance = 5,
+
+    hl_properties = { bg = "#2A2A37", underline = false },
+
+    pointer_style = "line region",
+
+    white_space_rendering = string.rep("\t\t\t\t\t", 50),
+
+    virtual_pointer_position = -4,
+    virtual_pointer_text = "@",
+
+    enable_autocmd = true,
+    autocmd_pattern = "*",
+  },
+}
