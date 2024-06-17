cb7d5df4a90f6ed0b44e9778c3ff7c01fa6fbfec
Simon Ho <simonho.ubc@gmail.com>
2024-06-17 01:43:10 -0700

    fix(nvim): pin codeium to previous version
    fixes neovim bug

--- nvim/lua/plugins/AI.lua
+++ nvim/lua/plugins/AI.lua
@@ -1,5 +1,7 @@
 M = {
   "Exafunction/codeium.vim", -- this is the .vim version of the plugin
+  enabled = true,
+  tag = "1.8.37",
   event = "InsertEnter",
   cmd = "Codeium",
   build = ":Codeium Auth",
