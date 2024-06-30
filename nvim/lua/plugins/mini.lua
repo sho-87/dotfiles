4c7171027e15179d2722d60c391240728d5510f4
Simon Ho <simonho.ubc@gmail.com>
2024-06-29 21:37:30 -0700

    feat(nvim): add custom textobjects

--- nvim/lua/plugins/mini.lua
+++ nvim/lua/plugins/mini.lua
@@ -14,6 +14,52 @@ return {
     },
   },
   {
+    "echasnovski/mini.ai",
+    opts = function(_, opts)
+      local ai = require("mini.ai")
+      opts.custom_textobjects = opts.custom_textobjects or {}
+
+      local mappings = {
+        ["/"] = { query = { a = "@comment.outer", i = "@comment.inner" }, desc = "Comment" },
+        ["v"] = { query = { a = "@assignment.outer", i = "@assignment.rhs" }, desc = "Variable" },
+        ["k"] = { pattern = "([%w_]+)%s*[:=]", desc = "Key" },
+        ["L"] = { pattern = "%l%l%l-://[A-Za-z0-9_%-/.#%%=?&'@+*]+", desc = "Link" },
+      }
+
+      -- Create new textobjects
+      local new_textobjects = {}
+      for k, v in pairs(mappings) do
+        if v.query then
+          new_textobjects[k] = ai.gen_spec.treesitter(v.query)
+        elseif v.pattern then
+          new_textobjects[k] = { v.pattern }
+        end
+      end
+      opts.custom_textobjects = vim.tbl_extend("force", opts.custom_textobjects, new_textobjects)
+
+      -- Register new textobjects with which-key
+      local i = {}
+      for k, v in pairs(mappings) do
+        i[k] = v.desc
+      end
+
+      local a = vim.deepcopy(i)
+      local root = vim.deepcopy(i)
+      for key, name in pairs({ n = "Next", l = "Last" }) do
+        i[key] = vim.tbl_extend("force", { name = "Inside " .. name .. " textobject" }, root)
+        a[key] = vim.tbl_extend("force", { name = "Around " .. name .. " textobject" }, root)
+      end
+
+      require("which-key").register({
+        mode = { "o", "x" },
+        i = i,
+        a = a,
+      })
+
+      return opts
+    end,
+  },
+  {
     "echasnovski/mini.animate",
     enabled = vim.g.neovide == nil,
     event = "VeryLazy",
