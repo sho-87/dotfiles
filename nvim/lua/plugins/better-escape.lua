cf6b5d54497f77c13fe858cbe74f38a64c7019d2
Simon Ho <simonho.ubc@gmail.com>
2024-07-09 01:15:26 -0700

    fix(nvim): temporary fix for escape bindings

--- nvim/lua/plugins/better-escape.lua
+++ nvim/lua/plugins/better-escape.lua
@@ -1,15 +1,45 @@
 return {
   "max397574/better-escape.nvim",
-  event = "InsertEnter",
+  event = "LazyFile",
   opts = {
     timeout = vim.o.timeoutlen,
     mappings = {
       i = {
+        j = {
+          j = false,
+        },
+        k = {
+          j = "<Esc>",
+        },
+      },
+      v = {
+        j = {
+          j = false,
+        },
+        k = {
+          j = "<Esc>",
+        },
+      },
+      c = {
+        j = {
+          j = false,
+        },
+        k = {
+          j = "<Esc>",
+        },
+      },
+      s = {
+        j = {
+          j = false,
+        },
         k = {
           j = "<Esc>",
         },
       },
       t = {
+        j = {
+          j = false,
+        },
         k = {
           j = "<Esc>",
         },
