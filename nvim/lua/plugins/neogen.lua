139650b3a227e4b5c760c653ffd1ebadaaa2fc3e
Simon Ho <simonho.ubc@gmail.com>
2024-07-21 11:15:03 -0700

    feat(nvim): add explicit neogen conventions

--- nvim/lua/plugins/neogen.lua
+++ nvim/lua/plugins/neogen.lua
@@ -4,4 +4,23 @@ return {
     local wk = require("which-key")
     wk.add({ "<leader>cn", icon = "", desc = "Neogen docs" })
   end,
+  opts = {
+    languages = {
+      python = {
+        template = {
+          annotation_convention = "google_docstrings",
+        },
+      },
+      typescript = {
+        template = {
+          annotation_convention = "tsdoc",
+        },
+      },
+      typescriptreact = {
+        template = {
+          annotation_convention = "tsdoc",
+        },
+      },
+    },
+  },
 }
