c1d35ddcf7678e7cf959f587ae0d534200f12a18
Simon Ho <simonho.ubc@gmail.com>
2024-07-29 22:33:21 -0700

    build(nvim): packages

--- nvim/lua/plugins/indents.lua
+++ nvim/lua/plugins/indents.lua
@@ -21,7 +21,7 @@ return {
         textobject = "",
         max_file_size = 1024 * 1024,
         error_sign = true,
-        exclude_filetypes = { neotest_summary = true, ["neo-tree-popup"] = true, lazyterm = true },
+        exclude_filetypes = { ["neotest-summary"] = true, ["neo-tree-popup"] = true, lazyterm = true },
       },
       indent = {
         enable = true,
