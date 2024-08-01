e0a84556771753627c6326cdbad7c4ce9f4f70e1
Simon Ho <simonho.ubc@gmail.com>
2024-08-01 03:37:46 -0700

    feat(nvim): add neogen keybinds

--- nvim/lua/plugins/neogen.lua
+++ nvim/lua/plugins/neogen.lua
@@ -4,7 +4,31 @@ return {
     local wk = require("which-key")
     wk.add({ "<leader>cn", icon = "", desc = "Neogen docs" })
   end,
+  keys = {
+    { "<leader>cn", vim.NIL },
+    {
+      "<leader>cnf",
+      "<cmd>lua require('neogen').generate({type = 'func'})<cr>",
+      desc = "Function",
+    },
+    {
+      "<leader>cnF",
+      "<cmd>lua require('neogen').generate({type = 'file'})<cr>",
+      desc = "File",
+    },
+    {
+      "<leader>cnc",
+      "<cmd>lua require('neogen').generate({type = 'class'})<cr>",
+      desc = "Class",
+    },
+    {
+      "<leader>cnt",
+      "<cmd>lua require('neogen').generate({type = 'type'})<cr>",
+      desc = "Type",
+    },
+  },
   opts = {
+    snippet_engine = "nvim",
     languages = {
       python = {
         template = {
