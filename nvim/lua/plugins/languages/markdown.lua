6c27db1d58f2607d2f0f1a20073a136115734450
Simon Ho <simonho.ubc@gmail.com>
2024-09-08 22:10:51 -0700

    feat(nvim): settings for render markdown

--- nvim/lua/plugins/languages/markdown.lua
+++ nvim/lua/plugins/languages/markdown.lua
@@ -11,11 +11,27 @@ return {
     "MeanderingProgrammer/markdown.nvim",
     opts = {
       heading = {
+        position = "inline",
+        sign = true,
+        signs = { "󰫎 " },
+        border = true,
         width = "full",
+        icons = { " 󰲡 ", " 󰲣 ", " 󰲥 ", " 󰲧 ", " 󰲩 ", " 󰲫 " },
       },
       code = {
-        width = "full",
+        sign = true,
+        style = "full",
+        width = "block",
+        min_width = 60,
         left_pad = 2,
+        language_pad = 2,
+      },
+      pipe_table = {
+        preset = "round",
+      },
+      sign = {
+        enabled = true,
+        highlight = "RenderMarkdownSign",
       },
     },
   },
