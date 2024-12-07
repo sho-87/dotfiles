208003555580adb4752ac8ab9d5964acb97df88b
Simon Ho <simonho.ubc@gmail.com>
2024-12-07 05:41:06 -0800

    feat(nvim): snacks config

--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -2,9 +2,30 @@ return {
   {
     "folke/snacks.nvim",
     opts = {
+      gitbrowse = {
+        notify = true,
+        remote_patterns = {
+          { "^(https?://.*)%.git$", "%1" },
+          { "^git@personal%-github%.com:(.+)%.git$", "https://github.com/%1" },
+          { "^git@(.+):(.+)%.git$", "https://%1/%2" },
+          { "^git@(.+):(.+)$", "https://%1/%2" },
+          { "^git@(.+)/(.+)$", "https://%1/%2" },
+          { "^ssh://git@(.*)$", "https://%1" },
+          { "^ssh://([^:/]+)(:%d+)/(.*)$", "https://%1/%3" },
+          { "^ssh://([^/]+)/(.*)$", "https://%1/%2" },
+          { "ssh%.dev%.azure%.com/v3/(.*)/(.*)$", "dev.azure.com/%1/_git/%2" },
+          { "^https://%w*@(.*)", "https://%1" },
+          { "^git@(.*)", "https://%1" },
+          { ":%d+", "" },
+          { "%.git$", "" },
+        },
+      },
       notifier = {
         margin = { top = 0, right = 1, bottom = 1 },
         style = "fancy",
+      },
+      quickfile = {
+        enabled = true,
       },
     },
   },
