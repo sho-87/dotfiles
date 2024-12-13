3ea0429ceeee46a3ad405a4c9d4684269c1f2c48
Simon Ho <simonho.ubc@gmail.com>
2024-12-13 13:50:36 -0800

    feat(nvim): remove mini animate

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -14,7 +14,6 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.coding.yanky" },
     { import = "lazyvim.plugins.extras.editor.dial" },
     { import = "lazyvim.plugins.extras.editor.telescope" },
-    { import = "lazyvim.plugins.extras.ui.mini-animate" },
     { import = "lazyvim.plugins.extras.vscode" },
     -- languages
     { import = "lazyvim.plugins.extras.lang.docker" },
