c17385b3d1fca6848d6f41983b57d00ea136b99d
Simon Ho <simonho.ubc@gmail.com>
2024-12-16 00:48:42 -0800

    feat(nvim): remove tiny code actions

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -77,24 +77,4 @@ return {
       wakeup_delay = 2000, --milliseconds
     },
   },
-  {
-    "rachartier/tiny-code-action.nvim",
-    dependencies = {
-      { "nvim-lua/plenary.nvim" },
-      { "nvim-telescope/telescope.nvim" },
-    },
-    event = "LspAttach",
-    config = function()
-      require("tiny-code-action").setup()
-    end,
-    keys = {
-      {
-        "<leader>ca",
-        function()
-          require("tiny-code-action").code_action()
-        end,
-        desc = "Code Action",
-      },
-    },
-  },
 }
