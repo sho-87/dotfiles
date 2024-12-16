27bd2ca137aa04ed7120075711e5799eb93bc5dd
Simon Ho <simonho.ubc@gmail.com>
2024-12-16 01:00:59 -0800

    chore(nvim): remove yanky history viewer

--- nvim/lua/plugins/yanky.lua
+++ nvim/lua/plugins/yanky.lua
@@ -4,13 +4,6 @@ return {
     { "<leader>p", vim.NIL },
     { "p", "<Plug>(YankyPutAfter)", desc = "Put after" },
     { "P", "<Plug>(YankyPutBeforeLinewise)", desc = "Put before" },
-    {
-      "<leader>y",
-      function()
-        require("telescope").extensions.yank_history.yank_history({})
-      end,
-      desc = "Yank History",
-    },
   },
   opts = {
     highlight = {
@@ -20,23 +13,6 @@ return {
     },
     preserve_cursor_position = {
       enabled = true,
-    },
-    picker = {
-      telescope = {
-        use_default_mappings = false,
-        mappings = {
-          i = {
-            ["<CR>"] = require("yanky.telescope.mapping").put("p"),
-            ["<C-d>"] = require("yanky.telescope.mapping").delete(),
-          },
-          n = {
-            ["<CR>"] = require("yanky.telescope.mapping").put("p"),
-            p = require("yanky.telescope.mapping").put("p"),
-            P = require("yanky.telescope.mapping").put("P"),
-            d = require("yanky.telescope.mapping").delete(),
-          },
-        },
-      },
     },
   },
 }
