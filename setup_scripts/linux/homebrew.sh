34c463f25ee0b84861089d3f87278d82a75bb405
Simon Ho <simonho.ubc@gmail.com>
2025-01-06 19:07:53 -0800

    feat(setup): add brew

---
+++ setup_scripts/linux/homebrew.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# https://brew.sh
+
+brew install bat
+brew install fd
+brew install ffmpeg
+brew install fzf
+brew install go
+brew install lazygit
+brew install neovim
+brew install node
+brew install nushell
+brew install pandoc
+brew install ripgrep
+brew install sqlite
+brew install starship
+brew install yazi sevenzip jq poppler imagemagick
+brew install zoxide
