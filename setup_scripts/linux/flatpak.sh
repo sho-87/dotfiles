d0d3d81a01b3d168a07fb0b486667382581569ec
Simon Ho <simonho.ubc@gmail.com>
2025-01-06 19:07:47 -0800

    feat(setup): add flatpak

---
+++ setup_scripts/linux/flatpak.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# https://flathub.org
+
+flatpak install flathub eu.betterbird.Betterbird -y
+flatpak install flathub com.google.Chrome -y
+flatpak install flathub com.discordapp.Discord -y
+flatpak install flathub com.dropbox.Client -y
+flatpak install flathub org.gimp.GIMP -y
+flatpak install flathub org.libreoffice.LibreOffice -y
+flatpak install flathub com.valvesoftware.Steam -y
