ad1ecca436470407267f4fe9ac204213bf43c556
Simon Ho <simonho.ubc@gmail.com>
2025-01-12 22:44:26 -0800

    feat(setup): applications for work

---
+++ setup_scripts/linux/work.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Applications for work
+
+flatpak install flathub com.slack.Slack
+
+brew install python@3.11
+brew install awscli@1
+
+brew tap hashicorp/tap
+brew install hashicorp/tap/terraform
