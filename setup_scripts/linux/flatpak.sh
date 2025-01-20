3f74612a382a56ae3cb18500d8fb9ae8c91f6d5d
Simon Ho <simonho.ubc@gmail.com>
2025-01-20 15:24:02 -0800

    feat(setup): add oversteer

--- setup_scripts/linux/flatpak.sh
+++ setup_scripts/linux/flatpak.sh
@@ -7,4 +7,5 @@ flatpak install flathub com.discordapp.Discord -y
 flatpak install flathub com.dropbox.Client -y
 flatpak install flathub org.gimp.GIMP -y
 flatpak install flathub org.libreoffice.LibreOffice -y
+flatpak install flathub io.github.berarma.Oversteer
 flatpak install flathub com.valvesoftware.Steam -y
