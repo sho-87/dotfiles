2bea6de5e568dff7bd0574d154a7159f373a56f1
Simon Ho <simonho.ubc@gmail.com>
2025-01-29 00:34:17 -0800

    feat(setup): add bruno alpaca dbeaver

--- setup_scripts/linux/flatpak.sh
+++ setup_scripts/linux/flatpak.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 # https://flathub.org
 
+flatpak install flathub com.jeffser.Alpaca -y
 flatpak install flathub eu.betterbird.Betterbird -y
+flatpak install flathub com.usebruno.Bruno
 flatpak install flathub com.google.Chrome -y
+flatpak install flathub io.dbeaver.DBeaverCommunity
 flatpak install flathub com.discordapp.Discord -y
 flatpak install flathub com.dropbox.Client -y
 flatpak install flathub org.gimp.GIMP -y
