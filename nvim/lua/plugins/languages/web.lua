b3556ef5e491824833e0d2d5e006b284246de456
Simon Ho <simonho.ubc@gmail.com>
2025-01-28 18:22:43 -0800

    fix(nvim): fix rest client maps

--- nvim/lua/plugins/languages/web.lua
+++ nvim/lua/plugins/languages/web.lua
@@ -1,18 +1,39 @@
+local wk = require("which-key")
+
 return {
   {
     "vuki656/package-info.nvim",
     dependencies = { "MunifTanjim/nui.nvim" },
     ft = { "json" },
     init = function()
-      local wk = require("which-key")
       wk.add({ "<localleader>n", group = "  npm" })
     end,
     keys = {
-      { "<localleader>nh", ":lua require('package-info').toggle()<CR>", ft = "json", desc = "Hide dependencies" },
-      { "<localleader>nd", ":lua require('package-info').delete()<CR>", ft = "json", desc = "Delete dependency" },
-      { "<localleader>nu", ":lua require('package-info').update()<CR>", ft = "json", desc = "Update dependency" },
+      {
+        "<localleader>nh",
+        ":lua require('package-info').toggle()<CR>",
+        ft = "json",
+        desc = "Hide dependencies",
+      },
+      {
+        "<localleader>nd",
+        ":lua require('package-info').delete()<CR>",
+        ft = "json",
+        desc = "Delete dependency",
+      },
+      {
+        "<localleader>nu",
+        ":lua require('package-info').update()<CR>",
+        ft = "json",
+        desc = "Update dependency",
+      },
       { "<localleader>nc", ":lua require('package-info').change_version()<CR>", ft = "json", desc = "Change version" },
-      { "<localleader>ni", ":lua require('package-info').install()<CR>", ft = "json", desc = "Install new dependency" },
+      {
+        "<localleader>ni",
+        ":lua require('package-info').install()<CR>",
+        ft = "json",
+        desc = "Install new dependency",
+      },
     },
     opts = {
       colors = {
@@ -23,6 +44,72 @@ return {
       hide_up_to_date = true,
       hide_unstable_versions = true,
       package_manager = "npm",
+    },
+  },
+  {
+    "mistweaverco/kulala.nvim",
+    ft = "http",
+    init = function()
+      wk.add({ "<localleader>r", group = " 󰖟 REST" })
+    end,
+    keys = {
+      { "<localleader>rb", "<cmd>lua require('kulala').scratchpad()<cr>", desc = "Open scratchpad", ft = "http" },
+      { "<localleader>rc", "<cmd>lua require('kulala').copy()<cr>", desc = "Copy as cURL", ft = "http" },
+      { "<localleader>rC", "<cmd>lua require('kulala').from_curl()<cr>", desc = "Paste from curl", ft = "http" },
+      {
+        "<localleader>rg",
+        "<cmd>lua require('kulala').download_graphql_schema()<cr>",
+        desc = "Download GraphQL schema",
+        ft = "http",
+      },
+      {
+        "<localleader>ri",
+        "<cmd>lua require('kulala').inspect()<cr>",
+        desc = "Inspect current request",
+        ft = "http",
+      },
+      {
+        "<localleader>rn",
+        "<cmd>lua require('kulala').jump_next()<cr>",
+        desc = "Jump to next request",
+        ft = "http",
+      },
+      {
+        "<localleader>rp",
+        "<cmd>lua require('kulala').jump_prev()<cr>",
+        desc = "Jump to previous request",
+        ft = "http",
+      },
+      {
+        "<localleader>rq",
+        "<cmd>lua require('kulala').close()<cr>",
+        desc = "Close window",
+        ft = "http",
+      },
+      {
+        "<localleader>rr",
+        "<cmd>lua require('kulala').replay()<cr>",
+        desc = "Replay the last request",
+        ft = "http",
+      },
+      {
+        "<localleader>rs",
+        "<cmd>lua require('kulala').run()<cr>",
+        desc = "Send the request",
+        ft = "http",
+      },
+      {
+        "<localleader>rS",
+        "<cmd>lua require('kulala').show_stats()<cr>",
+        desc = "Show stats",
+        ft = "http",
+      },
+      {
+        "<localleader>rt",
+        "<cmd>lua require('kulala').toggle_view()<cr>",
+        desc = "Toggle headers/body",
+        ft = "http",
+      },
     },
   },
   {
