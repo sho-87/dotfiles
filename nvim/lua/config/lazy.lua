63fc42575872c4449cb277ba73751c64fe8ea957
Simon Ho <simonho.ubc@gmail.com>
2025-01-30 11:03:21 -0800

    feat(nvim): remove kulala

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -19,7 +19,6 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.editor.fzf" },
     { import = "lazyvim.plugins.extras.editor.inc-rename" },
     { import = "lazyvim.plugins.extras.test.core" },
-    { import = "lazyvim.plugins.extras.util.rest" },
     { import = "lazyvim.plugins.extras.vscode" },
     -- languages
     { import = "lazyvim.plugins.extras.lang.docker" },
