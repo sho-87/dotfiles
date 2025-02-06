bc7e5577694f5c1b2cfb81040afa6c8b61e705ca
Simon Ho <simonho.ubc@gmail.com>
2025-02-06 07:56:12 -0800

    feat(nvim): hide dotfiles default

--- nvim/lua/plugins/neotree.lua
+++ nvim/lua/plugins/neotree.lua
@@ -117,7 +117,7 @@ return {
         },
         filtered_items = {
           visible = false, -- when true, they will just be displayed differently than normal items
-          hide_dotfiles = false,
+          hide_dotfiles = true,
           hide_gitignored = true,
           hide_hidden = true, -- only works on Windows for hidden files/directories
         },
