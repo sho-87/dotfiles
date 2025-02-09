69d27c516474c8a74ce023e94963130a3005908d
Simon Ho <simonho.ubc@gmail.com>
2025-02-09 01:52:06 -0800

    fix(nvim): remove some problematic binds

--- nvim/lua/plugins/fzf.lua
+++ nvim/lua/plugins/fzf.lua
@@ -38,8 +38,6 @@ return {
       files = {
         -- `file_edit_or_qf` opens a single selection or sends multiple selection to quickfix
         ["enter"] = actions.file_edit_or_qf,
-        ["ctrl-h"] = actions.toggle_hidden,
-        -- ["ctrl-i"] = actions.toggle_ignore, -- this overrides tab select
         ["ctrl-s"] = actions.file_split,
         ["ctrl-v"] = actions.file_vsplit,
         ["ctrl-t"] = actions.file_tabedit,
@@ -85,8 +83,6 @@ return {
       glob_separator = "%s%-%-", -- query separator pattern (lua): ' --'
       actions = {
         ["ctrl-g"] = { actions.grep_lgrep },
-        ["ctrl-h"] = { actions.toggle_hidden },
-        -- ["ctrl-i"] = { actions.toggle_ignore }, -- this overrides tab select
       },
     },
     lsp = {
