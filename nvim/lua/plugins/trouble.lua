53dd3f75db2a2ccdf865d24d89ceee645b177bcb
Simon Ho <simonho.ubc@gmail.com>
2025-02-11 02:17:52 -0800

    fix(nvim): fix trouble spec

--- nvim/lua/plugins/trouble.lua
+++ nvim/lua/plugins/trouble.lua
@@ -1,29 +1,31 @@
 local fs = require("utils.fs")
 
 return {
-  "folke/trouble.nvim",
-  init = function()
-    local wk = require("which-key")
-    wk.add({ "<leader>cS", name = "LSP references (Trouble)" })
-  end,
-  opts = {
-    focus = true, -- Focus the window when opened
-    follow = true, -- Follow the current item
-    indent_guides = true, -- show indent guides
-    modes = {
-      symbols = {
-        focus = true,
-        groups = {
-          { "filename", format = "{file_icon}{short_filename} {count}" },
+  {
+    "folke/trouble.nvim",
+    init = function()
+      local wk = require("which-key")
+      wk.add({ "<leader>cS", name = "LSP references (Trouble)" })
+    end,
+    opts = {
+      focus = true, -- Focus the window when opened
+      follow = true, -- Follow the current item
+      indent_guides = true, -- show indent guides
+      modes = {
+        symbols = {
+          focus = true,
+          groups = {
+            { "filename", format = "{file_icon}{short_filename} {count}" },
+          },
+          format = "{kind_icon}{symbol.name} {pos}",
+          win = { position = "right", size = 35 },
         },
-        format = "{kind_icon}{symbol.name} {pos}",
-        win = { position = "right", size = 35 },
       },
-    },
-    formatters = {
-      short_filename = function(ctx)
-        return { text = fs.shorten_path(ctx.item.dirname, 2) .. fs.get_path_sep() .. ctx.item.basename }
-      end,
+      formatters = {
+        short_filename = function(ctx)
+          return { text = fs.shorten_path(ctx.item.dirname, 1) .. fs.get_path_sep() .. ctx.item.basename }
+        end,
+      },
     },
   },
   {
