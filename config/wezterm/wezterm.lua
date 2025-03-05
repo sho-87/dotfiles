5d19ebdac5de4276bca0f5f79e81faa0b6554ed8
Simon Ho <simonho.ubc@gmail.com>
2025-03-05 04:46:57 -0800

    fix(wezterm): set window size as proportion

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -7,8 +7,12 @@ wezterm.on("gui-startup", function(cmd)
 	local _, _, window = wezterm.mux.spawn_window(cmd or {
 		workspace = "main",
 	})
-
-	window:gui_window():set_inner_size(1080, 600)
+	window:gui_window()
+	local screens = wezterm.gui.screens()
+	local proportion = 0.6
+	local width = math.floor(screens.active.width * proportion)
+	local height = math.floor(screens.active.height * proportion)
+	window:gui_window():set_inner_size(width, height)
 end)
 
 local is_windows = function()
